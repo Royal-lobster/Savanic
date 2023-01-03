330c3f43d4406031124b841518b3f3b432146a3a
Srujan Gurram <srujangs8@gmail.com>
2023-01-04 00:01:38 +0530

    added artist page under construction

---
+++ pages/artist/[id].tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from '@mantine/core';
+import React from 'react';
+
+const ArtistPage = () => (
+  <Center mt="30vh">
+    <Text size="xl"> 🚧 Artist Page Under Development</Text>
+  </Center>
+);
+
+export default ArtistPage;
