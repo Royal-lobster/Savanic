43c5a58035a033e6af03e8277c10b8733de76c68
Srujan Gurram <srujangs8@gmail.com>
2023-01-03 18:18:55 +0530

    refactored search page and made it mobile responsive

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,28 @@ import React, { useEffect } from 'react';
 import { shortenText } from '../utils/shortenText';
 import useSaavn, { PathOptions } from '../hooks/useSaavn';
 import { SearchResultCard } from '../components/search/SearchResultCard';
+import { Result } from '../types/searchAll';
+
+const SearchView = ({ results, title }: { results: Result[]; title: string }) => (
+  <>
+    <Title mt={20} order={2}>
+      {title}
+    </Title>
+    <Divider />
+    <SimpleGrid
+      cols={2}
+      breakpoints={[
+        { maxWidth: 980, cols: 2, spacing: 'md' },
+        { maxWidth: 760, cols: 1, spacing: 'sm' },
+      ]}
+      spacing={20}
+    >
+      {results.map((result) => (
+        <SearchResultCard result={result} />
+      ))}
+    </SimpleGrid>
+  </>
+);
 
 const search = () => {
   const router = useRouter();
@@ -26,52 +48,15 @@ const search = () => {
       {data.topQuery.results.map((result) => (
         <SearchResultCard result={result} />
       ))}
-
-      {data.songs.results.length !== 0 && (
-        <>
-          <Title mt={20} order={2}>
-            Songs
-          </Title>
-          <Divider />
-          <SimpleGrid cols={2} spacing={20}>
-            {data.songs.results.map((song) => (
-              <SearchResultCard result={song} />
-            ))}
-          </SimpleGrid>
-        </>
-      )}
+      {data.songs.results.length !== 0 && <SearchView results={data.songs.results} title="Songs" />}
       {data.artists.results.length !== 0 && (
-        <>
-          <Title order={2}>Artists</Title>
-          <Divider />
-          <SimpleGrid cols={2} spacing={20}>
-            {data.artists.results.map((artist) => (
-              <SearchResultCard result={artist} />
-            ))}
-          </SimpleGrid>
-        </>
+        <SearchView results={data.artists.results} title="Artists" />
       )}
       {data.albums.results.length !== 0 && (
-        <>
-          <Title order={2}>Albums</Title>
-          <Divider />
-          <SimpleGrid cols={2} spacing={20}>
-            {data.albums.results.map((album) => (
-              <SearchResultCard result={album} />
-            ))}
-          </SimpleGrid>
-        </>
+        <SearchView results={data.albums.results} title="Albums" />
       )}
       {data.playlists.results.length !== 0 && (
-        <>
-          <Title order={2}>Playlists</Title>
-          <Divider />
-          <SimpleGrid cols={2} spacing={20}>
-            {data.playlists.results.map((playlist) => (
-              <SearchResultCard result={playlist} />
-            ))}
-          </SimpleGrid>
-        </>
+        <SearchView results={data.playlists.results} title="Playlists" />
       )}
     </Stack>
   );
